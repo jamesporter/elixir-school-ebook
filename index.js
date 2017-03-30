8e1371c1dd1c6e4f8eb67f842bfa066693567efe
James Porter <james@amimetic.co.uk>
2017-03-30 18:23:06 +0100

    read in files, build data array, process markdown to html, read metadata

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-const frontMatter = require('front-matter'),
-      marked = require('marked'),
-      epub = require('epub-gen'),
-  fs = require('fs');
+const frontMatter = require("front-matter"),
+  marked = require("marked"),
+  epub = require("epub-gen"),
+  fs = require("fs");
+
+const config = JSON.parse(fs.readFileSync('config.json'));
+
+const loadContent = () => {
+
+  let data = [];
+
+  config.subdirectories.forEach( s => {
+    console.log("**", s);
+
+    const fileNames = fs.readdirSync(config.root + s);
+    fileNames.forEach( f => {
+      data.push(processRawFile(fs.readFileSync(config.root + s + "/" + f).toString()));
+    })
+  });
+  console.log(data);
+};
+
+const processRawFile = content => {
+
+  const {body, attributes} = frontMatter(content);
+
+  return {
+    attributes,
+    html: marked.parse(body)
+  }
+};
+
+
+loadContent();
